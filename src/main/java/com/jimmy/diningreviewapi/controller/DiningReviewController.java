6796a72db4bc5939c00b6b9fc0e171b7aec6ac05
sarasa0310 <dev.sarasa.0310@gmail.com>
2023-06-05 18:05:30 +0900

    Refactor: 다이닝 리뷰 리팩토링
    - 이미 승인된 다이닝 리뷰 승인 시도할 때 예외 발생하도록 변경
    - 다이닝 리뷰 제출 할 때 가공된 ResponseDto 응답 받도록 변경

--- src/main/java/com/jimmy/diningreviewapi/controller/DiningReviewController.java
+++ src/main/java/com/jimmy/diningreviewapi/controller/DiningReviewController.java
@@ -2,6 +2,7 @@ package com.jimmy.diningreviewapi.controller;
 
 import com.jimmy.diningreviewapi.domain.DiningReview;
 import com.jimmy.diningreviewapi.dto.DiningReviewRequest;
+import com.jimmy.diningreviewapi.dto.DiningReviewResponse;
 import com.jimmy.diningreviewapi.service.DiningReviewService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -21,9 +22,11 @@ public class DiningReviewController {
     ResponseEntity<?> submitDiningReview(@Valid @RequestBody DiningReviewRequest dto) {
         DiningReview submittedDiningReview = diningReviewService.submitDiningReview(dto);
 
+        DiningReviewResponse response = DiningReviewResponse.from(submittedDiningReview);
+
         return ResponseEntity.created(
                 URI.create("/dining-reviews/" + submittedDiningReview.getId()))
-                .body(submittedDiningReview);
+                .body(response);
     }
 
     @GetMapping
