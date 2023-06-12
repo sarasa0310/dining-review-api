662b270cd488d70034d3831e5638b495b09d7c53
sarasa0310 <dev.sarasa.0310@gmail.com>
2023-06-12 13:04:49 +0900

    Feat: 레스토랑 순위 조회 기능 구현
    - 레스토랑 평균 점수가 높은 순서대로 조회 가능
    - 페이지네이션 적용
    - 쿼리 성능을 높이기 위해 Restaurant에 averageScore 인덱스 추가

--- src/main/java/com/jimmy/diningreviewapi/repository/RestaurantRepository.java
+++ src/main/java/com/jimmy/diningreviewapi/repository/RestaurantRepository.java
@@ -1,6 +1,8 @@
 package com.jimmy.diningreviewapi.repository;
 
 import com.jimmy.diningreviewapi.domain.entity.Restaurant;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,5 +11,6 @@ public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 
     boolean existsByNameAndZipCode(String name, Integer zipCode);
     List<Restaurant> findAllByZipCodeOrderByIdDesc(Integer zipCode);
+    Page<Restaurant> findAllByOrderByAverageScoreDesc(Pageable pageable);
 
 }
