cfd4fc76ce260f4b182708ecdb6c726fefa061ba
jimmy <dev.sarasa.0310@gmail.com>
2023-09-08 20:42:00 +0900

    Chore: Querydsl을 사용하기 위해 JPAQueryFactory 빈 등록

--- src/main/java/com/jimmy/diningreviewapi/DiningReviewApiApplication.java
+++ src/main/java/com/jimmy/diningreviewapi/DiningReviewApiApplication.java
@@ -1,13 +1,22 @@
 package com.jimmy.diningreviewapi;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.persistence.EntityManager;
 
 @SpringBootApplication
 public class DiningReviewApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DiningReviewApiApplication.class, args);
+    }
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(EntityManager em) {
+        return new JPAQueryFactory(em);
     }
 
 }
