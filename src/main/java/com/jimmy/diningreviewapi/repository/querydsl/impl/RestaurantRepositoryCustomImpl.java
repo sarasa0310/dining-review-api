46af130c54ed814d7ed9dc41068e20d1d337bb72
jimmy <dev.sarasa.0310@gmail.com>
2023-09-16 20:53:36 +0900

    Feat: 레스토랑 동적 쿼리 검색 기능 추가
    - Querydsl을 활용해 동적인 쿼리를 쉽게 생성할 수 있음
    - Restaurant의 이름, 우편번호, 평균점수 이상, 이하 조건 검색을 동적으로 처리 가능

--- src/main/java/com/jimmy/diningreviewapi/repository/querydsl/impl/RestaurantRepositoryCustomImpl.java
+++ src/main/java/com/jimmy/diningreviewapi/repository/querydsl/impl/RestaurantRepositoryCustomImpl.java
@@ -1,14 +1,17 @@
 package com.jimmy.diningreviewapi.repository.querydsl.impl;
 
+import com.jimmy.diningreviewapi.dto.request.RestaurantSearch;
 import com.jimmy.diningreviewapi.dto.response.QRestaurantResponse;
 import com.jimmy.diningreviewapi.dto.response.RestaurantResponse;
 import com.jimmy.diningreviewapi.repository.querydsl.RestaurantRepositoryCustom;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.support.PageableExecutionUtils;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
 
@@ -56,6 +59,51 @@ public class RestaurantRepositoryCustomImpl implements RestaurantRepositoryCusto
                 .from(restaurant);
 
         return PageableExecutionUtils.getPage(content, pageable, countQuery::fetchOne);
+    }
+
+    @Override
+    public Page<RestaurantResponse> searchRestaurants(RestaurantSearch search, Pageable pageable) {
+        List<RestaurantResponse> content = queryFactory
+                .select(new QRestaurantResponse(
+                        restaurant.id, restaurant.name, restaurant.zipCode, restaurant.score, restaurant.averageScore))
+                .from(restaurant)
+                .where(
+                        restaurantNameEq(search.getRestaurantName()),
+                        restaurantZipCodeEq(search.getRestaurantZipCode()),
+                        avgScoreGoe(search.getAvgScoreGoe()),
+                        avgScoreLoe(search.getAvgScoreLoe())
+                )
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        JPAQuery<Long> countQuery = queryFactory
+                .select(restaurant.count())
+                .from(restaurant)
+                .where(
+                        restaurantNameEq(search.getRestaurantName()),
+                        restaurantZipCodeEq(search.getRestaurantZipCode()),
+                        avgScoreGoe(search.getAvgScoreGoe()),
+                        avgScoreLoe(search.getAvgScoreLoe())
+                );
+
+        return PageableExecutionUtils.getPage(content, pageable, countQuery::fetchOne);
+    }
+
+    private BooleanExpression restaurantNameEq(String restaurantNameCond) {
+        return StringUtils.hasText(restaurantNameCond) ? restaurant.name.eq(restaurantNameCond) : null;
+    }
+
+    private BooleanExpression restaurantZipCodeEq(Integer restaurantZipCodeCond) {
+        return restaurantZipCodeCond != null ? restaurant.zipCode.eq(restaurantZipCodeCond) : null;
+    }
+
+    private BooleanExpression avgScoreGoe(Integer avgScoreGoeCond) {
+        return avgScoreGoeCond != null ? restaurant.averageScore.goe(avgScoreGoeCond) : null;
+    }
+
+    private BooleanExpression avgScoreLoe(Integer avgScoreLoeCond) {
+        return avgScoreLoeCond != null ? restaurant.averageScore.loe(avgScoreLoeCond) : null;
     }
 
 }
